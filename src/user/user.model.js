8f10b8f4f9ace822da04f5dd56f0116a4fbbc8d0
jceferino-2022238 <jceferino-2022238@kinal.edu.gt>
2024-10-10 10:48:18 -0600

    User Model

---
+++ src/user/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "The name is required"]
+    },
+    email: {
+        type: String,
+        required : [true, "The email is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "The password is required"]
+    },
+    role: {
+        type: String,
+        enum: ["ADMIN_ROLE", "EDITOR_ROLE", "USER_ROLE"],
+        default: "USER_ROLE"
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    }
+})
+UserSchema.methods.toJSON = function(){
+    const {__v, password, _id, ...user}  = this.toObject();
+    user.uid = _id;
+    return user;
+}
+
+export default mongoose.model("User", UserSchema)
